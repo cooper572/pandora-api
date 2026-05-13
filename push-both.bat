77feabb30e9a731e5df46c9de1decfc72971b2c6
endoverdosing <endoverdosing@gmail.com>
2026-05-13 16:20:58 -0700

    Updated

--- push-both.bat
+++ push-both.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Pushing to GitHub...
-git push github main
+git push github main --force
 
 echo Pushing to Hugging Face...
-git push hf main
+git push hf main --force
 
 echo Done! Both repositories updated.
