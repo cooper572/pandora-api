46425ac7afe066accc035070b7fe7694a46575cb
endoverdosing <endoverdosing@gmail.com>
2026-05-21 20:21:16 -0700

    Updated

--- push-both.bat
+++ push-both.bat
@@ -1,4 +1,13 @@
 @echo off
+
+echo Setting up remotes...
+
+git remote remove github 2>nul
+git remote add github https://github.com/vyla-entertainment/stream-api
+
+git remote remove hf 2>nul
+git remote add hf https://huggingface.co/spaces/MissouriMonster/vyla
+
 echo Pushing to GitHub...
 git push github main --force
 
